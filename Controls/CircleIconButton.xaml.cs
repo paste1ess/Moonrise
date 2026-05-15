5072a0f1082ab13ad11825968dd8661c28dc380f
jamied <hey@isuckat.dev>
2026-05-15 00:09:01 -0400

    circleiconbutton takes properties now

---
+++ Controls/CircleIconButton.xaml.cs
@@ -0,0 +1,48 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Moonrise.Controls
+{
+    public sealed partial class CircleIconButton : UserControl
+    {
+        public string Glyph
+        {
+            get => (string)GetValue(GlyphProperty);
+            set => SetValue(GlyphProperty, value);
+        }
+
+        public static readonly DependencyProperty GlyphProperty =
+            DependencyProperty.Register(nameof(Glyph), typeof(string), typeof(CircleIconButton), new PropertyMetadata(string.Empty));
+
+        public double Size
+        {
+            get => (double)GetValue(SizeProperty);
+            set => SetValue(SizeProperty, value);
+        }
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register(nameof(Size), typeof(double), typeof(CircleIconButton), new PropertyMetadata(32.0));
+
+        public CircleIconButton()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
